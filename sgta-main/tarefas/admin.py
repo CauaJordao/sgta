5311cb725bf28642a2a74cabace9d118c7f4e32a
Cauã Jordão <cauajordao2311@gmail.com>
2026-06-08 15:57:21 -0300

    Delete sgta-main directory

--- sgta-main/tarefas/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Tarefas
-
-# Register your models here.
-admin.site.register(Tarefas)
--- sgta-main/usuarios/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.listar_usuarios),
-    path('<int:id>/', views.buscar_usuario_por_id),
-]
